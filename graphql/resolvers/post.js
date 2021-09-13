5765507977375ea1a31122ec0fb37ef427c54020
asonbede <asonbede@gmail.com>
2021-09-12 23:38:29 -0700

    create likes

--- graphql/resolvers/post.js
+++ graphql/resolvers/post.js
@@ -1,4 +1,5 @@
-const { AuthenticationError } = require("apollo-server");
+const { authenticationError,UserInputError } = require("apollo-server");
+const { argsToArgsConfig } = require("graphql/type/definition");
 const { create } = require("../../models/post");
 const Post = require("../../models/post");
 const checkAuth = require("../../util/check-auth");
@@ -28,7 +29,10 @@ module.exports = {
   Mutation: {
     async createPost(_, { body }, context) {
       const user = checkAuth(context);
-      console.log({ user });
+      if (args.body.trim()==="") {
+        throw new Error("Post body must not be empty")
+      }
+      //console.log({ user });
       const newPost = new Post({
         body,
         user: user.id,
@@ -52,5 +56,29 @@ module.exports = {
         throw new Error(err);
       }
     },
+    async likePost(_,{postId},context){
+      const {username} = checkAuth(context)
+      const post = await Post.findById(postId)
+      if (post) {
+        if (post.likes.find(like=>like.username===username)) {
+          //already liked
+          post.likes= post.likes.filter(like=>like.username !== username)
+          //await post.save()
+        }else{
+           //not liked, like post
+        post.likes.push({
+          username,
+          createdAt: new Date().toISOString()
+        })
+
+        }
+        await post.save()
+        return post
+      } else {
+       throw new UserInputError("Post not found")
+
+      }
+     
+    }
   },
 };
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { ApolloServer } = require("apollo-server");
+const { ApolloServer,PubSub } = require("apollo-server");
 const mongoose = require("mongoose");
 const { MONGODB } = require("./config.js");
 //const gql = require("graphql-tag");
@@ -8,7 +8,8 @@ const resolvers = require("./graphql/resolvers");
 // const resolvers = {
 
 // };
-
+//const pubsub = new  PubSub();
+ 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
